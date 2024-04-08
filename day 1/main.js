eab7854779a705515fe9ab4ae0fdef1e4c1d5eff
navin kumar <97692286+navinkrv@users.noreply.github.com>
2024-04-08 22:01:52 +0530

    Initial commit

---
+++ day 1/main.js
@@ -0,0 +1,72 @@
+/*
+Js topics : 
+1. data type
+    number
+    string -  sequence of char
+    boolean - true/false
+    undefined
+    null
+
+2. variables - a name that defines a space in memory(RAM)
+   - let/var/const
+    let b ;
+    let a = 1;
+    a =3;
+    console.log(a); // 3
+    console.log(b); // undefined;
+     const a= 2;
+     a = 1;
+
+3. operators : special symbol to perform certain operations eg : + - * /  * % && || !
+    let res = 20 / 2  // 10
+    let res = 20 % 2  // 0
+
+4. loops : TO PERFORM  TASK REPEATEDLY
+
+5. decision making
+6. functions
+7. array and strings
+8. object
+9. hoisting
+
+react - lib  
+1. react js - framework
+2. react native - framework
+
+
+
+*/
+
+let name = "ali"; // string
+let state = true; // bool
+let test = null; // null
+let obj = { name: "sdfsd" }; // object
+let test2;
+console.log(test2);
+
+// ###### operators ########
+let a = 2; // number
+let b = 3;
+
+let res = a + b;
+
+// simple intrest
+
+/*
+p*R*T/100
+*/
+
+let p = 1000;
+let r = 10;
+let t = 2;
+
+let result = (p * r * t) / 100;
+
+console.log(result);
+
+// HOMEwork
+// 1. to perform all basic calculation
+// 2. to find percentage of 1 student marks in 5 subjects
+// 3. to find compound intrest
+
+// Rules : Dont use Internet
